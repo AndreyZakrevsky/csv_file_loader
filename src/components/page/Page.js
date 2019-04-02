4d9132b6e74e3ff860959e29314ebcf0b3146dd4
Andey Zakrevsky <zak111revsky@gmail.com>
2019-04-02 20:09:02 +0300

    first commit

---
+++ src/components/page/Page.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+ import './page.scss';
+
+class Page extends Component {
+  render() {
+    return (
+      <div className="page">
+        <h1>Paggeeeee</h1>
+      </div>
+    );
+  }
+}
+
+export default Page;
